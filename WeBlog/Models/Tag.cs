944f30d171b3665d1bc81d950701af52cf58a14f
alexmonteil <alex.monteil@outlook.com>
2022-06-23 01:18:30 -0500

    Changed Author to BlogUser in all Models

--- WeBlog/Models/Tag.cs
+++ WeBlog/Models/Tag.cs
@@ -7,7 +7,7 @@ namespace WeBlog.Models
     {
         public int Id { get; set; }
         public int PostId { get; set; }
-        public string AuthorId { get; set; }
+        public string BlogUserId { get; set; }
 
         [Required]
         [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
@@ -15,6 +15,6 @@ namespace WeBlog.Models
 
         // Navigation Properties
         public virtual Post Post { get; set; }
-        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
     }
 }
